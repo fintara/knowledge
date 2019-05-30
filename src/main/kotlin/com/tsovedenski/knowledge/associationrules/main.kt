c922a63edf3cfc788673d8568c97f6062ae5f394
Tsvetan Ovedenski <dev@tsovedenski.com>
2019-05-30 11:09:36 +0200

    Fix apriori algorithm

--- src/main/kotlin/com/tsovedenski/knowledge/associationrules/main.kt
+++ src/main/kotlin/com/tsovedenski/knowledge/associationrules/main.kt
@@ -3,20 +3,25 @@ package com.tsovedenski.knowledge.associationrules
 /**
  * Created by Tsvetan Ovedenski on 2019-05-28.
  */
+typealias Pattern = List<Value>
+fun pattern(vararg values: Any): Pattern = values.map { when (it) {
+    is Int -> Value.Fixed(it)
+    else   -> Value.Any
+} }
+
 class Knowledge (vararg val knowledge: List<Int>) {
     val size = knowledge[0].size
     val rows = knowledge.size
 
     fun discoverRules(support: Double): List<Rule> {
-        val solutions = mutableListOf<Set<List<Value>>>()
-
-        tailrec fun go(patterns: Set<List<Value>>, solutions: List<Set<List<Value>>> = emptyList()): Set<List<Value>> {
+        tailrec fun go(patterns: Set<Pattern>, solutions: List<Set<Pattern>> = emptyList()): Set<Pattern> {
             val joined = patterns.join()
             val checked = joined
-                .map { Pair(it, it.split()) }
-                .filter { (_, splits) -> splits.all { (a, b) -> a in patterns && b in patterns } }
-                .map { it.first }
+                .filter { pattern -> pattern.subpatterns().all { it in patterns } }
             val next = checked.filter { supportFor(it) >= support }.toSet()
+
+            next.forEach(::println)
+            println()
 
             if (next.isEmpty()) {
                 return solutions.flatten().toSet()
@@ -35,43 +40,47 @@ class Knowledge (vararg val knowledge: List<Int>) {
 
             properties
                 .map { Rule(
-                    it.subList(0, pivot).mapIndexed { index, value -> if (value is Value.Fixed) index else null }.filterNotNull().toSet(),
-                    it.subList(pivot, size).mapIndexed { index, value -> if (value is Value.Fixed) index + pivot else null }.filterNotNull().toSet()
+                    it.subList(0, pivot).mapIndexed { index, value -> if (value is Value.Fixed) Equal(index, value.value) else null }.filterNotNull().toSet(),
+                    it.subList(pivot, size).mapIndexed { index, value -> if (value is Value.Fixed) Equal(index + pivot, value.value) else null }.filterNotNull().toSet()
                 ) }
                 .also { rules.addAll(it) }
         }
 
+        properties.forEach(::println)
+        println()
+
         return rules.filter { it.head.isNotEmpty() && it.tail.isNotEmpty() }
     }
 
-    fun getSimplePatterns(): Set<List<Value>> {
+    fun getSimplePatterns(): Set<Pattern> {
         val empty = List(size) { Value.Any }
         return (0 until size).flatMap { attr ->
             knowledge.getValues(attr).map { empty.fix(attr, it) }
         }.toSet()
     }
 
-    fun supportFor(row: List<Value>): Double {
+    fun supportFor(row: Pattern): Double {
         val den = knowledge.map { pairwise(it, row) }.sum().toDouble()
         return den / rows
     }
 
-    private fun pairwise(data: List<Int>, row: List<Value>): Int {
-        return data.zip(row).map { (a, b) ->
+    private fun pairwise(data: List<Int>, row: Pattern): Int {
+        val sum = data.zip(row).map { (a, b) ->
             when (b) {
                 is Value.Any -> 0
                 is Value.Fixed -> if (a == b.value) 1 else 0
             }
         }.sum()
+        return if (sum == row.degree) 1 else 0
     }
 
-    private fun List<Value>.fix(attribute: Int, value: Int): List<Value> {
+    private fun Pattern.fix(attribute: Int, value: Int): Pattern {
         val copy = toMutableList()
         copy[attribute] = Value.Fixed(value)
         return copy
     }
 
-    private fun List<Value>.unfix(attribute: Int): List<Value> {
+    private fun Pattern.unfix(attribute: Int): Pattern {
         val copy = toMutableList()
         copy[attribute] = Value.Any
         return copy
@@ -79,15 +88,15 @@ class Knowledge (vararg val knowledge: List<Int>) {
 
     private fun Array<out List<Int>>.getValues(attribute: Int) = map { it[attribute] } . distinct() . sorted()
 
-    private fun Set<List<Value>>.join(): Set<List<Value>> {
+    private fun Set<Pattern>.join(): Set<Pattern> {
         val pairs = zip(0 until size-1)
             .flatMap { (row, i) ->
                 (i+1 until size).map { j -> Pair(row, elementAt(j)) }
             }
 
-        fun join(first: List<Value>, second: List<Value>): List<Value>? {
+        fun join(first: Pattern, second: Pattern): Pattern? {
             return first.zip(second).map { (a, b) -> when {
-                a is Value.Fixed && b is Value.Fixed -> return null
+                a is Value.Fixed && b is Value.Fixed -> if (a.value == b.value) a else return null
                 a is Value.Fixed -> a
                 b is Value.Fixed -> b
                 else -> Value.Any
@@ -97,13 +106,13 @@ class Knowledge (vararg val knowledge: List<Int>) {
         return pairs.map { (a, b) -> join(a, b) }.filterNotNull().toSet()
     }
 
-    private fun List<Value>.split(): Set<Pair<List<Value>, List<Value>>> {
+    private fun Pattern.split(): Set<Pair<Pattern, Pattern>> {
         val currentDegree = degree
         if (currentDegree < 2) {
             return emptySet()
         }
 
-        val splits = mutableListOf<Pair<List<Value>, List<Value>>>()
+        val splits = mutableListOf<Pair<Pattern, Pattern>>()
         val pattern = MutableList(size) { false }
         var pivot = 0
 
@@ -127,6 +136,15 @@ class Knowledge (vararg val knowledge: List<Int>) {
             .toSet()
     }
 
+    private fun Pattern.subpatterns(): Set<Pattern> {
+        val currentDegree = degree
+        if (currentDegree < 2) {
+            return emptySet()
+        }
+
+        return (0 until size).map { unfix(it) }.filter { it.degree == currentDegree - 1 }.toSet()
+    }
+
     private fun <A, B> List<Pair<A, B>>.invert(): Pair<List<A>, List<B>> {
         val firsts = mutableListOf<A>()
         val seconds = mutableListOf<B>()
@@ -148,7 +166,7 @@ fun main() {
         listOf(3,3,1),
         listOf(2,1,1)
     )
-
+    
 //    val patterns = knowledge.getSimplePatterns()
 //    patterns.forEach {
 //        println("$it (${knowledge.supportFor(it)})")
@@ -183,8 +201,22 @@ sealed class Value {
 
 val Iterable<Value>.degree: Int get() = filterIsInstance<Value.Fixed>().size
 
-data class Rule (val head: Set<Int>, val tail: Set<Int>) {
-    constructor(head: Int, tail: Set<Int>) : this(setOf(head), tail)
-    constructor(head: Set<Int>, tail: Int) : this(head, setOf(tail))
-    constructor(head: Int, tail: Int) : this(setOf(head), setOf(tail))
+data class Equal(val attribute: Int, val value: Int) {
+    override fun toString(): String {
+        return "x($attribute)=$value"
+    }
+}
+
+data class Rule (val head: Set<Equal>, val tail: Set<Equal>) {
+    constructor(head: Equal, tail: Set<Equal>) : this(setOf(head), tail)
+    constructor(head: Set<Equal>, tail: Equal) : this(head, setOf(tail))
+    constructor(head: Equal, tail: Equal) : this(setOf(head), setOf(tail))
+
+    override fun toString(): String {
+        val p = head.joinToString(" ∧ ")
+        val implies = "⇒"
+        val q = tail.joinToString(" ∧ ")
+
+        return "$p $implies $q"
+    }
 }
